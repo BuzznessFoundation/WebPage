3b5a0841ce5364d314033dad9b7d50131a46cf25
Cristian Alvarado <cristian.alvarado@usach.cl>
2025-03-27 13:35:18 -0300

    Change package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "https://buzznessfoundation.github.io/",
+  base: mode === "production" ? "https://buzznessfoundation.github.io/" : "/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
